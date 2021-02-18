2e5a6143c9b3deecea3cfe13249ce834c3f1e2d6
Alaina Hicks <ajhicks0816@gmail.com>
2021-02-18 17:34:53 -0600

    js code & links

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-$('#hhhdeployed').attr('href','https://spicyjackrabbits.github.io/theFoodApp/');
-$('#wdsdeployed').attr('href','https://ayejayhicks.github.io/workdayscheduler/');
-$('#crdeployed').attr('href','https://ayejayhicks.github.io/coderefactor/');
+$('#project1').on('click', ()=> {
+    console.log('clicked');
+    var win = window.open('https://spicyjackrabbits.github.io/theFoodApp', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+});
+$('#workdayscheduler').on('click', ()=> {
+    console.log('clicked');
+    var win = window.open('https://ayejayhicks.github.io/workdayscheduler/', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+});
+$('#coderefactor').on('click', ()=> {
+    console.log('clicked');
+    var win = window.open('https://ayejayhicks.github.io/coderefactor/', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+});
+
